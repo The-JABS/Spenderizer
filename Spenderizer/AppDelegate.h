6bea65bf92ea30655d5c74f27b6fdafd4ad43832
imachumphries <imachumphries@me.com>
2016-03-22 16:35:56 -0400

    First commit

---
+++ Spenderizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spenderizer
+//
+//  Created by Benjamin Humphries on 3/22/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spenderizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spenderizer
+//
+//  Created by Benjamin Humphries on 3/22/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
