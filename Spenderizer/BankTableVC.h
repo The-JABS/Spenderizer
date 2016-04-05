ea28b7975a38945bacb9ceee93fb916ee833f159
imachumphries <imachumphries@me.com>
2016-04-05 16:16:50 -0400

    Added stub for listing bank accounts

---
+++ Spenderizer/BankTableVC.h
@@ -0,0 +1,15 @@
+//
+//  BankTableVC.h
+//  Spenderizer
+//
+//  Created by Benjamin Humphries on 4/5/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BankTableVC : UITableViewController {
+    
+}
+
+@end
