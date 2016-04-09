99afac34acd0ee8be978632a2809600479295b0d
imachumphries <imachumphries@me.com>
2016-04-09 00:01:30 -0400

    Added simple model class for OFX Sign on Query

---
+++ Spenderizer/OFXSignOnQuery.h
@@ -0,0 +1,16 @@
+//
+//  OFXSignOnQuery.h
+//  Spenderizer
+//
+//  Created by Benjamin Humphries on 4/8/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OFXQuery.h"
+#import "Bank.h"
+#import "User.h"
+
+@interface OFXSignOnQuery : OFXQuery
+
+@end
---
+++ Spenderizer/OFXSignOnQuery.m
@@ -0,0 +1,29 @@
+//
+//  OFXSignOnQuery.m
+//  Spenderizer
+//
+//  Created by Benjamin Humphries on 4/8/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import "OFXSignOnQuery.h"
+
+@implementation OFXSignOnQuery
+
+- (id)initWithBank:(Bank *)_bank user:(BankAccount *)_bankAccount {
+    if (self = [super init]) {
+        [self.body setString:[OFXUtil loadQueryFromFile:@"OFXSignOnQueryTemplate"]];
+        
+        [self replace:@"{DTCLIENT}" with:[OFXUtil getDate]];
+        [self replace:@"{USERID}" with:[_bankAccount userID]];
+        [self replace:@"{USERPASS}" with:[_bankAccount password]];
+        [self replace:@"{LANGUAGE}" with:@"ENG"];
+        [self replace:@"{ORG}" with:[_bank org]];
+        [self replace:@"{FID}" with:[_bank fid]];
+        [self replace:@"{APPID}" with:@"QWIN"];
+        [self replace:@"{APPVER}" with:@"2200"];
+    }
+    return self;
+}
+
+@end
