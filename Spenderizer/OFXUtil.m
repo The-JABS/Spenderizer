ca5163d5db3b9e08778f4a9e0c35a6f01dc50866
imachumphries <imachumphries@me.com>
2016-04-09 00:02:05 -0400

    Added OFXUtil class for any repeated functions

---
+++ Spenderizer/OFXUtil.m
@@ -0,0 +1,27 @@
+//
+//  OFXUtil.m
+//  Spenderizer
+//
+//  Created by Benjamin Humphries on 4/5/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import "OFXUtil.h"
+#import "OFXQuery.h"
+
+@implementation OFXUtil
+
++ (NSString *)getDate {
+    NSDateFormatter* df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"yyyyMMddhhmmss"];
+    return [df stringFromDate:[NSDate date]];
+}
+
++ (NSString *)loadQueryFromFile:(NSString *)fileName {
+    NSString *path = [[NSBundle mainBundle] pathForResource:fileName ofType:@""];
+    NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    content = [content stringByReplacingOccurrencesOfString:@" " withString:@""];
+    return content;
+}
+
+@end
