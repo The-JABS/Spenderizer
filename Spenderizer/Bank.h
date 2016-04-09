dcd3f99534ee80c53f0fbd78a13c1f96e9846256
imachumphries <imachumphries@me.com>
2016-04-09 02:36:34 -0400

    Added constructor to Bank along with extra instance variables

--- Spenderizer/Bank.h
+++ Spenderizer/Bank.h
@@ -11,6 +11,8 @@
 @interface Bank : NSObject {
     
 }
+- (id)initWithID:(NSString *)_ID name:(NSString *)_name fid:(NSString *)_fid
+             org:(NSString *)_org url:(NSString *)_url brokerID:(NSString *)_brokerID;
 
 @property (nonatomic, retain) NSString *ID;         // Bank's OFX ID
 @property (nonatomic, retain) NSString *name;       // Banks name
@@ -18,5 +20,11 @@
 @property (nonatomic, retain) NSString *org;
 @property (nonatomic, retain) NSString *url;        // url of bank's OFX Server
 @property (nonatomic, retain) NSString *brokerID;   // Not sure if this is needed
+
+// Extra info may be needed for round up savings.
+@property (nonatomic, retain) NSString *address;
+@property (nonatomic, retain) NSString *city;
+@property (nonatomic, retain) NSString *state;
+@property (nonatomic, retain) NSString *postalCode;
 
 @end
