0a59fbe81386583cb2c219e2405b70fe92953d6f
Jackson Foley <jfoley16@radford.edu>
2016-04-10 22:47:56 -0400

    Added slide out menu. Need to some how add the bar button item to all of the VC's

---
+++ Spenderizer/NavigationTVC.h
@@ -0,0 +1,14 @@
+//
+//  NavigationTVC.h
+//  Spenderizer
+//
+//  Created by Jackson Foley on 4/10/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//https://www.youtube.com/watch?v=5SUV1YY2yxQ
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationTVC : UITableViewController
+
+@end
---
+++ Spenderizer/NavigationTVC.m
@@ -0,0 +1,62 @@
+//
+//  NavigationTVC.m
+//  Spenderizer
+//
+//  Created by Jackson Foley on 4/10/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import "NavigationTVC.h"
+
+@interface NavigationTVC ()
+
+@end
+
+@implementation NavigationTVC {
+    NSArray *menu;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    menu = @[@"home", @"settings", @"accounts"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return [menu count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *cellIdentifier = [menu objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    
+    
+    return cell;
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
