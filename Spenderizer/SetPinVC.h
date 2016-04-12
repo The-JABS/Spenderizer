2bb208f46906637099f60e22fe539067bec1d1f4
Jackson Foley <jfoley16@radford.edu>
2016-04-12 18:52:18 -0400

    Made settingsVC and connected it to the SetPinVC

--- Spenderizer/SetPinVC.h
+++ Spenderizer/SetPinVC.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "ABPadLockScreenSetupViewController.h"
 
-@interface SetPinVC : UIViewController<ABPadLockScreenSetupViewControllerDelegate> {
+@interface SetPinVC : UITableViewController<ABPadLockScreenSetupViewControllerDelegate> {
     
 }
 
---
+++ Spenderizer/SettingsVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingsVC.h
+//  Spenderizer
+//
+//  Created by Jackson Foley on 4/12/16.
+//  Copyright © 2016 The JABS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsVC : UITableViewController
+
+@end
